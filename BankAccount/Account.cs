c57b4f576e7286deeaa1d85a85c85fcdc94ab5ea
Alexandra Haley <72041575+Tinet0624@users.noreply.github.com>
2021-08-21 12:22:40 -0700

    Add project files.

---
+++ BankAccount/Account.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccount
+{
+    /// <summary>
+    /// Represents a single checking account
+    /// </summary>
+    public class Account
+    {
+        /// <summary>
+        /// Get the current balance. Read only property!
+        /// </summary>
+        public double Balance { get; private set; }
+
+        /// <summary>
+        /// Deposits the amount in the bank account and returns new balance.
+        /// </summary>
+        /// <param name="amt">The amount to deposit</param>
+        public double Deposit(double amt)
+        {
+            if (amt >= 10000)
+            {
+                throw new ArgumentException($"{nameof(amt)} must be smaller than 10,000.");
+            }
+            if (amt <= 0)
+            {
+                throw new ArgumentOutOfRangeException($"{nameof(amt)} is not a valid deposit amount.");
+            }
+            Balance += amt;
+            return Balance;
+        }
+
+        public void Withdraw(double amt)
+        {
+            if (amt > Balance)
+            {
+                throw new ArgumentException($"You can not withdraw more thatn the current balance.");
+            }
+            if (amt < 0)
+            {
+                throw new ArgumentException($"You can not withdraw a negative amount.");
+            }
+            Balance -= amt;
+        }
+    }
+}
---
+++ BankAccountTests/AccountTests.cs
@@ -0,0 +1,142 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BankAccount;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccount.Tests
+{
+    [TestClass()]
+    public class AccountTests
+    {
+        private Account acc;
+
+        [TestInitialize] // run code before each test
+        public void Initialize()
+        {
+            acc = new Account();
+        }
+
+        //---------------------------------------Deposit--------------------------------------
+
+        [TestMethod]
+        [TestCategory("Deposit")]
+        [DataRow(10_000)]
+        [DataRow(10_000.01)]
+        [DataRow(double.MaxValue)]
+        public void Deposit_ToLarge_ThrowsArgException(double tooLargeDeposit)
+        {
+            Assert.ThrowsException<ArgumentException>(() => acc.Deposit(tooLargeDeposit));
+        }
+
+        [TestMethod()]
+        [TestCategory("Deposit")]
+        [DataRow(100)]
+        [DataRow(9999.99)]
+        [DataRow(.01)]
+        public void Deposit_PositiveAmount_AddsToBalance(double initialDeposit)
+        {
+            // AAA - Arrange Act Assert
+            // Arrange - Create variabls and objects
+            const double startBalance = 0;
+
+            // Act - Execute method under test
+            acc.Deposit(initialDeposit);
+
+            // Assert - check condition
+            Assert.AreEqual(startBalance + initialDeposit, acc.Balance);
+        }
+
+        [TestMethod()]
+        [TestCategory("Deposit")]
+        public void Deposit_PositiveAmount_ReturnsUpdatedBalance()
+        {
+            // Arrange
+            double startBalance = 0;
+            double depositAmount = 10.55;
+
+            // Act
+            double newBalance = acc.Deposit(depositAmount);
+
+            // Assert
+            double expectedBalance = startBalance + depositAmount;
+            Assert.AreEqual(expectedBalance, newBalance);
+        }
+
+        // Tests
+        // Multiple deposits
+        [TestMethod]
+        [TestCategory("Deposit")]
+        public void Deposit_MultipleAmounts_ReturnsAccumulatedBalance()
+        {
+            // Arrange
+            double dep1 = 10;
+            double dep2 = 25;
+            double expectedBalance = dep1 + dep2;
+
+            // Act
+            double intermediateBalance = acc.Deposit(dep1);
+            double finalBalance = acc.Deposit(dep2);
+
+            // Assert
+            Assert.AreEqual(dep1, intermediateBalance);
+            Assert.AreEqual(expectedBalance, finalBalance);
+
+        }
+
+        // Neg Deposits
+        [TestMethod]
+        [TestCategory("Deposit")]
+        public void Deposit_NegAmount_ThrowsArgumentException()
+        {
+            // Arrange
+            double negDeposit = -1;
+
+            // Assert & Act
+            Assert.ThrowsException<ArgumentOutOfRangeException>
+                (
+                    // Act
+                    () => acc.Deposit(negDeposit)
+                );
+        }
+
+        //---------------------------------------Withdraw-------------------------------------
+
+        [TestMethod]
+        [TestCategory("Withdraw")]
+        [DataRow(100, 50)]
+        [DataRow(50, 50)]
+        [DataRow(9.99, 9.99)]
+        public void Withdraw_PositiveAmt_SubtractsFromBalance(double initialDeposit, double withdrawAmount)
+        {
+            double expectedBalance = initialDeposit - withdrawAmount;
+
+            acc.Deposit(initialDeposit);
+            acc.Withdraw(withdrawAmount);
+
+            Assert.AreEqual(expectedBalance, acc.Balance);
+        }
+
+        [TestMethod]
+        [TestCategory("Withdraw")] // Make this data driven!
+        public void Withdraw_MoreThanBalance_TrowsArgumentException()
+        {
+            Account myAccount = new Account();
+            double withdrawAmount = 1000;
+            Assert.ThrowsException<ArgumentException>(() => myAccount.Withdraw(withdrawAmount));
+        }
+
+        [TestMethod]
+        [TestCategory("Withdraw")]
+        public void Withdraw_NegAmt_ThrowsArgumentException()
+        {
+            double negWithdraw = -1;
+
+            Assert.ThrowsException<ArgumentException>(() => acc.Withdraw(negWithdraw));
+        }
+
+    }
+
+}
